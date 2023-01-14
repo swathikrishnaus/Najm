28284f955b17446e99f803c4f16abb7d572bb93a
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-01-14 17:47:15 +0530

    added

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ $(document).ready(function(){
   })
 })
 
+
+
+
+
+// -----------------navbar---------------------
+
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 50)
+        $(".navbar").addClass("nav-shadow")
+    else
+        $(".navbar").removeClass("nav-shadow")
+})
+
+// -----------------navbar---------------------
